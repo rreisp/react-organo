45a45e4cf627e81d6a1c73e832c897befca2e63a
Rodrigo Reis <rreisp@gmail.com>
2022-11-08 16:33:12 -0300

    Controlando o estado dos componentes com useState

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder} ...`
+    // let valor = ''
+    const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
         </div>
     )
 }
