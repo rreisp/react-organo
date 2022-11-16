f611b162497db7a704ff79787208962ed67b791a
Rodrigo Reis <rreisp@gmail.com>
2022-11-16 15:42:36 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/rreisp.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>Rodrigo</h4>
+            <h5>CEO</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
